dae7934baa1339ab2a3ba7a7931d19c3e072f863
hongjunpyo11 <jpyo98@naver.com>
2023-01-28 22:27:46 +0900

    객체 조립시에 부품으로 path variable 도 사용된다.

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -212,7 +212,7 @@ public class MainController {
         return p;
     }
 
-    @GetMapping("addPerson")
+    @GetMapping("/addPerson/{id}")
     @ResponseBody
     Person addPerson(Person p) {
         return p;
