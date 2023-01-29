edabc86d4ff281803a5ccde3195cd10e6eb0268d
hongjunpyo11 <jpyo98@naver.com>
2023-01-29 22:35:28 +0900

    Answer 만드는 방법을 간소화, 더 객체지향 적으로 수정

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -45,15 +45,11 @@ public class AnswerRepositoryTests {
         a1.setContent("sbb는 질문답변 게시판 입니다.");
         a1.setCreateDate(LocalDateTime.now());
         q.addAnswer(a1);
-        answerRepository.save(a1);
 
         Answer a2 = new Answer();
         a2.setContent("sbb에서는 주로 스프링부트관련 내용을 다룹니다.");
         a2.setCreateDate(LocalDateTime.now());
         q.addAnswer(a2);
-        answerRepository.save(a2);
-
-        q.getAnswerList().add(a2);
 
         questionRepository.save(q);
     }
@@ -63,11 +59,18 @@ public class AnswerRepositoryTests {
     @Rollback(false)
     void save() {
         Question q = questionRepository.findById(2).get();
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setCreateDate(LocalDateTime.now());
-        q.addAnswer(a);
-        answerRepository.save(a);
+
+        Answer a1 = new Answer();
+        a1.setContent("네 자동으로 생성됩니다.");
+        a1.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a1);
+
+        Answer a2 = new Answer();
+        a2.setContent("네네~ 맞아요!");
+        a2.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a2);
+
+        questionRepository.save(q);
     }
 
     @Test
